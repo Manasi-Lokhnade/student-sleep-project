d7b986459c089a1cdd9b3d4ab91866f7bfeb5ba0
Manasi Lokhande <143057830+Manasi-Lokhnade@users.noreply.github.com>
2025-11-25 01:14:54 +0530

    Delete predict.py

--- predict.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import pandas as pd
-
-class Predictor:
-    def __init__(self, model_path):
-        self.model = joblib.load(model_path)
-
-    def predict(self, df: pd.DataFrame):
-        return self.model.predict(df)
